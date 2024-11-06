71a3b42e89778ccbf86a3530c0a15cb62066afb9
luxao <lukas.lobll@gmail.com>
2024-11-06 10:28:20 +0100

    Delete jquery, update getNames script

--- getNames.js
+++ getNames.js
@@ -1,7 +1,32 @@
-var names = []
+javascript:
+var names = [];
 
-Array.from(document.querySelector("#content_value > table > tbody > tr > td:nth-child(1) > table:nth-child(3)").childNodes[1].children).forEach(
+cssClassesLuxao = `
+<style>
+    #tribe-names-wrapper {
+        width: 300px;
+        height: auto;
+        background-color: #fff;
+        color: #000;
+        border: 2px solid #000;
+        margin: 15px 15px;
+    }
+</style>`
+
+$("#contentContainer").eq(0).prepend(cssClassesLuxao);
+$("#mobileHeader").eq(0).prepend(cssClassesLuxao);
+
+var dataDiv = document.createElement("div")
+dataDiv.id = "tribe-names-wrapper"
+
+
+Array.from($("#content_value > table > tbody > tr > td:nth-child(1) > table:nth-child(3)")[0].childNodes[1].children).forEach(
     item => {if (typeof(item.children[0].children[1]) != 'undefined') {
         names.push(item.children[0].children[1].textContent.trim())
     }
 })
+
+dataDiv.innerText = names.join(";")
+
+$("#contentContainer").prepend(dataDiv);
+$("#mobileHeader").prepend(dataDiv);
